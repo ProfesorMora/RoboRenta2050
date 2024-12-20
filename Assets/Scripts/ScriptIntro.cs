cf0210cb765a330735bf568a011f7ce56e2fda19
ProfesorMora <algst@tuta.io>
2024-12-20 16:41:04 +0100

    refactor(scenes): swith to adressing scenes by name (again)

--- Assets/Scripts/ScriptIntro.cs
+++ Assets/Scripts/ScriptIntro.cs
@@ -25,7 +25,7 @@ public class ScriptIntro : MonoBehaviour
         {
             if (alphaValue < 0)
             {
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene("formulario");
             }   
             if (fadeTime > 0)
             {
